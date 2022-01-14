822febdcbbb86576cc7e479b1f1f8f4f4901a833
Kim <faeriekim3@gmail.com>
2022-01-14 15:44:33 +0000

    improved layout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget, QSpinBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget, QSpinBox
 
 from random import choice
 
@@ -30,25 +30,58 @@ class MainWindow(QMainWindow):
         multiplylabel =  QLabel("X")
         input8 = QSpinBox()
         self.button = QPushButton("Calculate")
+
+        prompt1.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt2.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt3.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt4.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt5.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt6.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        prompt7.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
         
         #the app layout
         layout = QVBoxLayout()
-        layout.addWidget(prompt1)
-        layout.addWidget(input1)
-        layout.addWidget(prompt2)
-        layout.addWidget(input2)
-        layout.addWidget(prompt3)
-        layout.addWidget(input3)
-        layout.addWidget(prompt4)
-        layout.addWidget(input4)
-        layout.addWidget(prompt5)
-        layout.addWidget(input5)
-        layout.addWidget(prompt6)
-        layout.addWidget(input6)
-        layout.addWidget(prompt7)
-        layout.addWidget(input7)
-        layout.addWidget(multiplylabel)
-        layout.addWidget(input8)
+
+        question1 = QHBoxLayout()
+        question1.addWidget(prompt1)
+        question1.addWidget(input1)
+
+        question2 = QHBoxLayout()
+        question2.addWidget(prompt2)
+        question2.addWidget(input2)
+
+        question3 = QHBoxLayout()
+        question3.addWidget(prompt3)
+        question3.addWidget(input3)
+
+        question4 = QHBoxLayout()
+        question4.addWidget(prompt4)
+        question4.addWidget(input4)
+
+        question5 = QHBoxLayout()
+        question5.addWidget(prompt5)
+        question5.addWidget(input5)
+
+        question6 = QHBoxLayout()
+        question6.addWidget(prompt6)
+        question6.addWidget(input6)
+
+        question7 = QHBoxLayout()
+        question7.addWidget(prompt7)
+        question7.addWidget(input7)
+        question7.addWidget(multiplylabel)
+        question7.addWidget(input8)
+        
+        #nesting layouts
+        layout.addLayout(question1)
+        layout.addLayout(question2)
+        layout.addLayout(question3)
+        layout.addLayout(question4)
+        layout.addLayout(question5)
+        layout.addLayout(question6)
+        layout.addLayout(question7)
+        
         layout.addWidget(self.button)
 
         container = QWidget()
