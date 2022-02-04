ce01e39d5b05cc0911b1d9580f48db5fb27a4c3b
Kim <faeriekim3@gmail.com>
2022-02-04 14:52:43 +0000

    added comments explaining my calculations

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ class MainWindow(QMainWindow):
         floorResult = QLabel("")
         roofText = QLabel("The amount of blocks you'll likely need for the roof are: ")
         roofResult = QLabel("")
+        windowText = QLabel("The amount of panes for windows is: ")
+        windowResult = QLabel("")
 
         
         #the app layout
@@ -90,19 +92,26 @@ class MainWindow(QMainWindow):
         windowsAnswer.addWidget(input8)
 
         result = QVBoxLayout()
+
         wallSummary = QHBoxLayout()
         result.addLayout(wallSummary)
         wallSummary.addWidget(wallText)
         wallSummary.addWidget(wallResult)
+
         floorSummary = QHBoxLayout()
         result.addLayout(floorSummary)
         floorSummary.addWidget(floorText)
         floorSummary.addWidget(floorResult)
+
         roofSummary = QHBoxLayout()
         result.addLayout(roofSummary)
         roofSummary.addWidget(roofText)
         roofSummary.addWidget(roofResult)
 
+        windowSummary = QHBoxLayout()
+        result.addLayout(windowSummary)
+        windowSummary.addWidget(windowText)
+        windowSummary.addWidget(windowResult)
         
         #nesting layouts
         layout.addLayout(question1)
@@ -127,21 +136,37 @@ class MainWindow(QMainWindow):
         #the function of the app
         
         def Button_pressed():
+            # window height x window width x number of windows
             windowCalculation = input7.value() * input8.value() * input6.value()
 
+            # number of doors x 2 for wall calculation (doors are 2 blocks high)
             doorCalculation = input5.value() * 2
 
+            # wall width and wall depth are each multiplied by wall height, but also
+            # each one is minus 1 because of sharing blocks with the other faces.
+            # Times each one by 2 and add them together because there are four walls.
+            # Also windows and doors are subtracted from the wall calculation
+            # so that we're just counting wall blocks.
             singleFace1 = (input2.value() -1) * input1.value() 
             singleFace2 = (input3.value() - 1) * input1.value()
             wallCalculation = (singleFace1 * 2) + (singleFace2 * 2) - windowCalculation - doorCalculation
             
+            # wall width and wall depth are used to calculate the floor area
+            # of the building. (minus 2 because it's inside dimensions).
+            # This is then used to calculate the floor blocks by multiplying by the amount
+            # of floors and also adding the amount of doors (because you need a floor block
+            # under a door).
             area = (input2.value() - 2) * (input3.value() - 2)
             floorCalculation = (area * input4.value()) + input5.value()
+            # the roof however takes the wall width and wall depth and adds 2 before 
+            # multiplying them together.  Adding 2 is because of the lip that most roofs
+            # have.
             roofCalculation = (input2.value() + 2) * (input3.value() + 2)
-            
+
             wallResult.setText(str(wallCalculation))
             floorResult.setText(str(floorCalculation))
             roofResult.setText(str(roofCalculation))
+            windowResult.setText(str(windowCalculation))
         
 
         calculate.clicked.connect(Button_pressed)
